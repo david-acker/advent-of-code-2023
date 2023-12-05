5e3dfbc5c5b89aa26d47c9a76c752e56c95a424d
David Acker <davidacker3@gmail.com>
2023-12-04 22:09:53 -0500

    Day 3: Gear Ratios

--- src/day3/main.py
+++ src/day3/main.py
@@ -1,14 +1,18 @@
 import sys
 from typing import List
 
-from solution import get_part_one_result
+from solution import get_part_one_result, get_part_two_result
 from utils import get_input_lines
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         raise Exception("Must provide an input file")
+    
+    input_lines = get_input_lines(sys.argv[1])
 
     # Part One
-    part_one_input: List[str] = get_input_lines(sys.argv[1])
-    print(f"Part One: {get_part_one_result(part_one_input)}")
+    print(f"Part One: {get_part_one_result(input_lines)}")
+
+    # Part Two
+    print(f"Part Two: {get_part_two_result(input_lines)}")
--- src/day3/solution.py
+++ src/day3/solution.py
@@ -16,14 +16,59 @@ def get_part_one_result(schematic: List[str]) -> int:
                 or not has_adjacent_symbol(schematic, x, y)):
                 continue
 
-            part_number = get_part_number(schematic, x, y, visited)
-
-            print(part_number)
-
-            part_number_sum += part_number
+            part_number_sum += get_part_number(schematic, x, y, visited)
 
     return part_number_sum
 
+
+def get_part_two_result(schematic: List[str]) -> int:
+
+    gear_ratio_sum = 0
+    visited: List[List[bool]] = [[False for _ in range(len(schematic[0]))] for _ in range(len(schematic))]
+
+    for x in range(len(schematic)):
+        for y in range(len(schematic[0])):
+
+            if (schematic[x][y] == "*"):
+                gear_ratio = try_get_gear_ratio(schematic, x, y, visited)
+
+                if (gear_ratio is not None):
+                    gear_ratio_sum += gear_ratio
+
+    return gear_ratio_sum
+
+
+def try_get_gear_ratio(schematic: List[str], x: int, y: int, visited: List[List[bool]]) -> int | None:
+    part_numbers: List[int] = []
+
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if (len(part_numbers) > 2):
+                return None
+
+            new_x = x + i
+            new_y = y + j
+
+            if (new_x == x and new_y == y):
+                continue
+
+            if (new_x < 0 
+                or new_x >= len(schematic) 
+                or new_y < 0 
+                or new_y >= len(schematic[0])):
+                continue
+
+            if (visited[new_x][new_y] 
+                or not is_number(schematic[new_x][new_y])):
+                continue
+
+            part_numbers.append(get_part_number(schematic, new_x, new_y, visited))
+
+    if (len(part_numbers) != 2):
+        return None
+    
+    return part_numbers[0] * part_numbers[1]
+    
 
 def has_adjacent_symbol(schematic: List[str], x: int, y: int) -> bool:
 
